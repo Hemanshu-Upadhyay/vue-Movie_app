efab33069c5ac6dbffb20dabeb892981de5507c3
Hemanshu <upadhyayhri@gmail.com>
2022-09-28 17:40:20 +0530

    Initial Commit

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-gray-800">
+    <div class="container mx-auto text-gray-400 py-4 px-8 flex justify-center">
+      Copyright &copy; 2022
+    </div>
+  </div>
+</template>
